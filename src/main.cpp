f552a7716a3398dc12439a8c96b8c93fb2343f95
starless333 <davidrodrocks2007@gmail.com>
2024-07-10 10:00:12 -0500

    Zen mode is completely working. Need to go see an opera but will be back later. Reminder: you need to fix the print_centered() method as it isn't doing its job lol. After that it should be pretty smooth sailing

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 // const definitions
 #define BUFFER_SIZE 15
 #define FILE "../misc/words.dat"
-
-
+#define LIMIT 10
+#define WORDS_SIZE 100
 using namespace std;
 
 
@@ -16,32 +16,29 @@ void zen(void);
 void timer(void);
 void word(void);
 void settings(void);
+void stats(int words, float time);
 
 
+WINDOW *window;
+const char BS = static_cast<char>(8);
 int row, col;
 vector<string> most_common;
 
 int main(void)
 {
 
-
     //make sure that prior to calling menu that the screen is clear. ie call clear() prior to menu() call.
     get_words();
-    initscr();
+    window = initscr();
+    raw();
     cbreak();
-
     menu();
-
-
     getmaxyx(stdscr, row, col);
-
-
-
     endwin();
+
+
     return EXIT_SUCCESS;
 }
-
-
 
 void get_words(void) {
     ifstream file(FILE);
@@ -54,6 +51,18 @@ void get_words(void) {
         most_common.push_back(contents);
     }
     file.close();
+}
+
+// Source: https://ubuntuforums.org/showthread.php?t=1626888
+// big thanks to them for the code!
+void backspace()
+{
+  nocbreak();
+  getyx(window, row, col);
+  move(row, col-1);
+  delch();
+  cbreak();
+  refresh();
 }
 
 void print_centered(const char *text) {
@@ -92,10 +101,40 @@ void menu() {
 }
 void zen(){
     clear();
-    addstr("This is zen mode. Type freely and once you press Esc your zen stats shall show!\n");
+    addstr("This is zen mode. Type freely and once you press ` your zen stats shall show!\n\n");
+    noecho();
+
+    bool get = true;
+    clock_t start_time, end_time;
+    bool run = true;
+    int words = 0;
+    char prev = ' ', c;
+    while(run) {
+        c = static_cast<char>(getch());
+        if(get) { start_time = clock(); get = false;}
+
+        switch(c) {
+            case static_cast<char>(127):
+            case BS: {
+                backspace();
+                break;
+            }
+            case '`': {
+                end_time = clock();
+                run = false;
+                break;
+            }
+            default: addch(c);
+        }
+        if(c == ' ' && prev != ' ') words++;
+        prev = c;
+    }
+    if(c != ' ') words++;
+
+    clear();
+    stats(words, float(end_time-start_time));
     getch();
 }
-
 //***********************************************************888*/
 void timer() {
     erase();
@@ -111,9 +150,32 @@ void timer() {
 //***********************************************************888*/
 void word(){
     clear();
+    random_device rd; // obtain a random number from hardware
+    mt19937 gen(rd()); // seed the generator
+    uniform_int_distribution<> distr(0,WORDS_SIZE-1); // define the range
+    // distr(gen) will produce a random number in that range.
+
+    string test;
+
+
+    for(int i = 0; i < LIMIT; i++ ) {
+        string partial = most_common[distr(gen)] + " ";
+        test.append(partial);
+    }
+    test.append("\n");
+
+    printw(test.c_str());
+    refresh();
+    getch();
+
+    return;
 }
 //***********************************************************888*/
 void settings(){
     clear();
 }
 //***********************************************************888*/
+void stats(int words, float time) {
+    printw("WORDS: %d\nTIME: %.2fs\n", words, time/1000.0f);
+    printw("WORDS PER MINUTE: %.2f\n", static_cast<float> (words)/(time/1000.0f)*60);
+}
