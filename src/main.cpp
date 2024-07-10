3499c6b5604a4b00d24f7d4a1ea803e570f11106
starless333 <davidrodrocks2007@gmail.com>
2024-07-10 10:49:26 -0500

    added support for word mode. This is still very buggy but i think there has been great progressclearclear Opera in a bit so this might be the last commit of the day...

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,53 @@
 #include<ncurses.h>
 #include<bits/stdc++.h>
+
+
 // const definitions
 #define BUFFER_SIZE 15
-#define FILE "../misc/words.dat"
 #define LIMIT 10
 #define WORDS_SIZE 100
+
+// doing this for ease of typing. I know it isn't good practice (see https://github.com/starless333/gentemp !);
 using namespace std;
 
 
 // function definitions;
-void menu(void);
-void print_centered(const char*);
-void get_words(void);
-void zen(void);
-void timer(void);
-void word(void);
-void settings(void);
-void stats(int words, float time);
+void menu(void); // prints the menu.
+void print_centered(const char*); // needs to be fixed admittedly.
+void zen(void); // zen mode implementation.
+void timer(void); // timer mode implementation.
+void word(void); // word mode implementation.
+void settings(void); // Still contemplating how to implement.
+void stats(int words, float time); // displays those juicy stats.
+
+// just some stuff that will be needed globally.
+WINDOW *window; // for backspace();
+const char BS = static_cast<char>(8); // for ease of use.
+int row, col; // for dimensions of terminal if ever needed.
 
 
-WINDOW *window;
-const char BS = static_cast<char>(8);
-int row, col;
-vector<string> most_common;
+
+// Thanks to https://www.espressoenglish.net/the-100-most-common-words-in-english/ for a list of 100 words. May not be accurate.
+vector<string> most_common = {"time", "year", "people", "way", "day", "man", "thing", "woman", "life", "child", "world", "school", "state", "family", "student", "group", "country", "problem", "hand", "part", "place", "case", "week", "company", "system", "program", "question", "work", "government", "number", "night", "point", "home", "water", "room", "mother", "area", "money", "story", "fact", "month", "lot", "right", "study", "book", "eye", "job", "word", "business", "issue", "side", "kind", "head", "house", "service", "friend", "father", "power", "hour", "game", "line", "end", "member", "law", "car", "city", "community", "name", "president", "team", "minute", "idea", "kid", "body", "information", "back", "parent", "face", "others", "level", "office", "door", "health", "person", "art", "war", "history", "party", "result", "change", "morning", "reason", "research", "girl", "guy", "moment", "air", "teacher", "force", "education"};
 
 int main(void)
 {
-
     //make sure that prior to calling menu that the screen is clear. ie call clear() prior to menu() call.
-    get_words();
     window = initscr();
-    raw();
-    cbreak();
-    menu();
-    getmaxyx(stdscr, row, col);
-    endwin();
-
-
-    return EXIT_SUCCESS;
-}
-
-void get_words(void) {
-    ifstream file(FILE);
-    string contents;
-    if(!file.is_open()) {
-        cerr << "could not open file.\n";
+    if(!has_colors()) {
+        cerr << "your terminal does not support colors. If you don't care about the colors simply remove this line in main.cpp.\n";
         exit(EXIT_FAILURE);
     }
-    while(file >> contents) {
-        most_common.push_back(contents);
-    }
-    file.close();
+    start_color();
+    raw();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    init_pair(2, COLOR_RED, COLOR_BLACK);
+    cbreak();
+    getmaxyx(stdscr, row, col);
+    menu();
+    endwin();
+
+    return EXIT_SUCCESS;
 }
 
 // Source: https://ubuntuforums.org/showthread.php?t=1626888
@@ -99,6 +96,8 @@ void menu() {
         }
     }
 }
+
+// Zen mode is practically fully functioning!;
 void zen(){
     clear();
     addstr("This is zen mode. Type freely and once you press ` your zen stats shall show!\n\n");
@@ -150,6 +149,7 @@ void timer() {
 //***********************************************************888*/
 void word(){
     clear();
+    noecho();
     random_device rd; // obtain a random number from hardware
     mt19937 gen(rd()); // seed the generator
     uniform_int_distribution<> distr(0,WORDS_SIZE-1); // define the range
@@ -162,12 +162,62 @@ void word(){
         string partial = most_common[distr(gen)] + " ";
         test.append(partial);
     }
-    test.append("\n");
 
     printw(test.c_str());
     refresh();
-    getch();
+    addch('\n');
 
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    init_pair(2, COLOR_RED, COLOR_BLACK);  
+
+    //RAW = 10/(time/1000)*60;
+    //WPM = RAW*accuracy/i;
+    //i = test.size() by the end of iteration;
+    int i = 0;
+    int accuracy = 0;
+    char c;
+    bool run = true, get = true;
+    clock_t start_time, end_time;
+    while(run) {
+        if(i == static_cast<int>(test.size()-1))   {
+            end_time = clock();
+            break;
+        }
+        c = static_cast<char>(getch());
+        if(get) { start_time = clock(); get = false;}
+
+        switch(c) {
+            case static_cast<char>(127):
+            case BS: {
+                backspace();
+                break;
+            }
+            case '`': {
+                end_time = clock();
+                run = false;
+                break;
+            }
+            // by default we compare the current character to the
+            default: {
+                if(c == test[i]) {
+                    accuracy++;
+                    wattron(window,COLOR_PAIR(1));
+                    waddch(window,c);
+                    wattroff(window,COLOR_PAIR(1));
+                } else {
+                    wattron(window,COLOR_PAIR(2));
+                    waddch(window,c);
+                    wattroff(window,COLOR_PAIR(2));
+                }
+            }
+        }
+        wrefresh(window);
+        i++;
+    }
+
+    clear();
+    stats(10, float(end_time-start_time));
+    getch();
     return;
 }
 //***********************************************************888*/
